ee7cfc7bb5dc71bea7ef27629f3518cbda04972d
j-lin <j-lin@click-sec.com>
2023-05-22 19:12:22 +0900

    dont need branching cases

--- builder.sh
+++ builder.sh
@@ -8,16 +8,8 @@ if [ -z "$VERSION" ]
         exit 1
 fi
 
-case $VERSION in
-    stg)
-        TAG="fh-all.com/bengal-images:${VERSION}-stg"
-        PROXY_ARG="--build-arg http_proxy=http://10.80.0.238:9999 --build-arg https_proxy=http://10.80.0.238:9999"
-        ;;
-    *)
-        TAG="fh-all.com/bengal-images:${VERSION}"
-        PROXY_ARG="--build-arg http_proxy=http://10.50.1.192:9999 --build-arg https_proxy=http://10.50.1.192:9999"
-        ;;
-esac
+TAG="fh-all.com/bengal-images:${VERSION}"
+PROXY_ARG="--build-arg http_proxy=http://10.50.1.192:9999 --build-arg https_proxy=http://10.50.1.192:9999"
 
 sudo docker build ${PROXY_ARG} -t ${TAG} .
 sudo docker save $TAG | sudo k3s ctr images import -
